0c79711df32a4db775a290533c70256f9779b320
anak subuh <johanesseptiawan2828@gmail.com>
2026-04-26 23:18:03 +0700

    Auto Update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ========== KONFIGURASI WEBHOOK GOOGLE SHEETS ==========
 // Ganti dengan URL Web App dari Google Apps Script ANDA!
-const GOOGLE_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycby2Rz64IZ6ZYp0L89hWEKku7i6M68aQlp6L-9XpI9ta-15JtLNqreBaOkqAUk_DgtkH/exec';
+const GOOGLE_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbwqbwobbKuucN_X4p9th5kV5fQFQpWZMGKF88swGqd3r6-sQCfG0T01QROqR0FQuFlK/exec
 
 // Konfigurasi GitHub (hanya untuk hosting file statis)
 const GITHUB_CONFIG = {
