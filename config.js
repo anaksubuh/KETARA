c9c1e67354f2eb096e65c08a293be20a4dffccb3
anak subuh <johanesseptiawan2828@gmail.com>
2026-04-26 23:32:51 +0700

    Auto Update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ========== KONFIGURASI ==========
-const GOOGLE_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycby2Rz64IZ6ZYp0L89hWEKku7i6M68aQlp6L-9XpI9ta-15JtLNqreBaOkqAUk_DgtkH/exec';
+const GOOGLE_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbyD0pwLcgb01fuTe9N55P1O_27A5Zax6tK8U19LO0a4HeEwHvexes-HdkMELUk1k0Ag/exec';
 
 console.log('Webhook URL:', GOOGLE_WEBHOOK_URL);
